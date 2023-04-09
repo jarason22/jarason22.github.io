f744389f406be0d407a7a71f9a343402c56b89f9
jarason22 <129004500+jarason22@users.noreply.github.com>
2023-04-09 17:03:16 -0500

    added shadows and mouse effects

--- cesar.js
+++ cesar.js
@@ -1,22 +1,65 @@
 import * as THREE from "https://cdnjs.cloudflare.com/ajax/libs/three.js/r108/three.module.js";
 // /import { init } from "./cesarCube.js"
 
-import { init } from "./cesarImages.js"
+import { init } from "./cesarImages.js";
 
 init();
 
-var cards = document.querySelectorAll('.cardff');
+const shadow = (e, c) => {
+  let { clientX: x, clientY: y } = e;
+  let { top: t, right: r, bottom: b, left: l } = c.getBoundingClientRect();
+  let xCenter = (r - l) / 2 + l;
+  let yCenter = (b - t) / 2 + t;
 
-[...cards].forEach((card)=>{
-  card.addEventListener( 'click', function() {
-    card.classList.toggle('is-flipped');
+  let xWalk = Math.round((x - xCenter) / 15);
+  let yWalk = Math.round((y - yCenter) / 15);
+
+  let engText = e.currentTarget.querySelector(".eng");
+  let spanText = e.currentTarget.querySelector(".span");
+
+  engText.style.textShadow = `
+    ${-xWalk}px ${-yWalk}px 10px #1985A1
+  `;
+
+  spanText.style.textShadow = `
+    ${-xWalk}px ${-yWalk}px 10px #E56399
+  `;
+
+  let img = e.currentTarget.querySelector(".imgBx");
+  img.style.boxShadow = `
+    ${-xWalk}px ${-yWalk}px 10px #363636
+  `;
+};
+
+const transformElement = (e, c) => {
+  let { clientX: mX, clientY: mY } = e;
+  let { height: h, width: w, x, y } = c.getBoundingClientRect();
+  let calcX = -((mY) - y - h / 2) / 15;
+  let calcY = (mX - x - w / 2) / 10;
+
+  let img = e.currentTarget.querySelector(".imgBx");
+  img.style.transform =
+    "rotateX(" + calcX + "deg) " + "rotateY(" + calcY + "deg) translateX(-50%) translateZ(100px)";
+};
+
+var cards = document.querySelectorAll(".card");
+
+[...cards].forEach((c) => {
+  const move = (e) => {
+    shadow(e, c);
+    transformElement(e, c);
+  };
+
+  c.addEventListener("click", function () {
+    c.classList.toggle("is-flipped");
   });
-});
 
-var cardx = document.querySelectorAll('.card');
+  c.addEventListener("mouseleave", function () {
+    c.classList.remove("is-flipped");
 
-[...cardx].forEach((c)=>{
-  c.addEventListener( 'click', function() {
-    c.classList.toggle('is-flipped');
+    let img = c.querySelector(".imgBx");
+    img.style.transform = "translateX(-50%)";
   });
+
+  c.addEventListener("mousemove", move);
 });
